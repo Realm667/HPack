40aa5284c5a27001b568d4be87c35663e1ef8488
gez <gez@cea0443f-7052-4e12-97ab-4c9f4b97172d>
2010-12-31 19:00:43 +0000

    - Removing duplicated files.

--- Decompilers/setup-py2exe.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-import py2exe
-
-setup(console=[{'script': 'listacs.py',
-               'icon_resources': [(1, 'zdoom.ico')],
-               'bundle-files': 1
-                }],
-      zipfile='python/library.zip')
