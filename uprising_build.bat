69bc9223d31e208afe868abbae3a0dbb2ff09efd
Xaser Acheron <xaser@angryscience.net>
2022-07-23 13:07:06 -0500

    exclude maps/#removed folder when building pk3

--- uprising_build.bat
+++ uprising_build.bat
@@ -1,3 +1,3 @@
 @echo off
-7za a -tzip -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" duprising_rc.pk3 .\pk3\*
+7za a -tzip -x!".git" -xr!"#removed" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" duprising_rc.pk3 .\pk3\*
 
