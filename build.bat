1a9334018e65ff454572935671b09db6771fb270
Ozymandias81 <Ozymandias81@gmx.com>
2023-11-05 13:49:49 +0100

    Minor update
    GRAPHICS & BUILDER
    
    :new: Put aside short boss healthbar to not clash it with larger one
    :new: Updated 7z and build batch (PSD, fonts and vscode comes all from Re-Releases repository, but once Torm will return here things might change and so those might be needed after all) - Also put no compression for pk3 (mx0)

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 @echo off
-7za a -tzip -x!".git" -xr!"#removed" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" duprising_rc.pk3 .\pk3\*
-
+7za a -tzip -mx0 -x!".git" -xr!"#removed" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!tools -xr!#PSD -x!.vscode\ -xr!"*.bat" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" duprising_rc.pk3 .\pk3\*
