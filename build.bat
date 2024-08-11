707fac8a3af6a5088cb9801ca1b360250af5fa6b
Ozymandias81 <Ozymandias81@gmx.com>
2024-08-11 19:59:24 +0200

    Batch file and 7za update
    :new: Make sure to exclude local zip/rar files from the built pk3
    :new: Uploaded new version of 7za

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-7za a -tzip -mx0 -x!".git" -xr!"#removed" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!tools -xr!#PSD -x!.vscode\ -xr!"*.bat" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" duprising_rc.pk3 .\pk3\*
+7za a -tzip -mx0 -x!".git" -xr!"#removed" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!tools -xr!#PSD -x!.vscode\ -xr!"*.bat" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf"-xr!"*.rar" -xr!"*.zip" duprising_rc.pk3 .\pk3\*
